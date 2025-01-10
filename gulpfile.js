10ba5976aa8773eb6e2773377f1f2c518c3ede7f
Aleksandr <aleksanrcarik86@gmail.com>
2025-01-10 18:49:19 +0500

    add prettier

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
-const { src, dest, watch, parallel, series } = require("gulp");
-const scss = require("gulp-sass")(require("sass"));
-const concat = require("gulp-concat");
-const uglify = require("gulp-uglify-es").default;
-const browserSync = require("browser-sync").create();
-const clean = require("gulp-clean");
-// const autoprefixer = require("gulp-autoprefixer");
+const { src, dest, watch, parallel, series } = require('gulp');
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
+
+const fonter = require('gulp-fonter');
+const ttf2woff2 = require('gulp-ttf2woff2');
+
+// const avif = require("gulp-avif");
+// const webp = require("gulp-webp");
+// const imagemin = require("gulp-imagemin");
+const newer = require('gulp-newer'); //--чистка кэша
+const svgSprite = require('gulp-svg-sprite');
+//const autoprefixer = require("gulp-autoprefixer");
 
 // const pug = require("gulp-pug");
 
@@ -18,61 +27,101 @@ const clean = require("gulp-clean");
 //     .pipe(dest("./dist"));
 // };
 
+//----- картинки
+// function images() {
+//   return src([
+//     'app/assets/img/*.*', '!app/assets/img/svg/*.svg'
+//   ])
+//     .pipe(newer('app/assets/img/dist'))
+//     .pipe(avif({quality: 50}))
+
+//     .pipe(src('app/assets/img/*.*'))
+//     .pipe(newer('app/assets/img/dist'))
+//     .pipe(webp())
+
+//     .pipe(src('app/assets/img/*.*'))
+//     .pipe(newer('app/assets/img/dist'))
+//     .pipe(imagemin())
+
+//     .pipe(dest('app/assets/img/dist'))
+// }
+
+function sprite() {
+  return src('app/assets/dist/*.svg');
+}
+
+//----шрифты
+function fonts() {
+  return src('app/assets/fonts')
+    .pipe(fonter({
+        formats: ['woff', 'ttf ']
+      }))
+    .pipe(src('app/fonts/*.ttf'))
+    .pipe(ttf2woff2())
+    .piipe(dest('app/assets/fonts'));
+}
+
 //---- стили
 function styles() {
-  return src("app/assets/style/style.scss")
-    // .pipe(autoprefixer({ overrideBrowsersList: ["last 10 version"] }))
-    .pipe(concat("style.min.css"))
-    .pipe(scss({ outputStyle: "compressed" }))
-    .pipe(dest("app/assets/style"))
-    .pipe(browserSync.stream());
+  return (
+    src('app/assets/style/style.scss')
+      //.pipe(autoprefixer({ overrideBrowserslist: ["last 10 version"] }))
+      .pipe(concat('style.min.css'))
+      .pipe(scss({ outputStyle: 'compressed' }))
+      .pipe(dest('app/assets/style'))
+      .pipe(browserSync.stream())
+  );
 }
 
 //---- scripts
 function scripts() {
-  return src(["node_modules/swiper/swiper-bundle.js", "app/assets/js/*.js", '!app/assets/js/main.min.js'])
-    .pipe(concat("main.min.js"))
+  return src([
+    'node_modules/swiper/swiper-bundle.js',
+    'app/assets/js/*.js',
+    '!app/assets/js/main.min.js',
+  ])
+    .pipe(concat('main.min.js'))
     .pipe(uglify())
-    .pipe(dest("app/assets/js"))
+    .pipe(dest('app/assets/js'))
     .pipe(browserSync.stream());
 }
 
-
-function browsersync() {
+function watching() {
   browserSync.init({
     server: {
-      baseDir: "app/",
+      baseDir: 'app/',
     },
   });
-}
-
-function watching() {
-  watch(["app/assets/style/style.scss"], styles);
-  watch(["app/assets/js/main.js"], scripts);
-  watch(["app/*.html"]).on("change", browserSync.reload);
+  watch(['app/assets/style/style.scss'], styles);
+  //watch(["app/assets/img"], images);
+  watch(['app/assets/js/main.js'], scripts);
+  watch(['app/*.html']).on('change', browserSync.reload);
   //   watch(["app/**/*.html"]).on('change'. browserSync.reload); если html имеет несколько вложений, он все перероет
 }
 
 function cleanDist() {
-  return src('dist')
-    .pipe(clean())
+  return src('dist').pipe(clean());
 }
 
 function building() {
-    return src([
-        'app/assets/style/style.min.css',
-        'app/assets/js/main.min.js',
-        'app/**/*.html'
-    ], {base: 'app'})
-    .pipe(dest('dist'))
+  return src(
+    [
+      'app/assets/style/style.min.css',
+      'app/assets/js/main.min.js',
+      'app/assets/img/dist/*.*',
+      'app/**/*.html',
+    ],
+    { base: 'app' }
+  ).pipe(dest('dist'));
 }
 
-
 exports.styles = styles;
+exports.fonts = fonts;
 exports.scripts = scripts;
-exports.watching = watching;
-exports.browsersync = browsersync;
+//exports.images = images;
+exports.sprite = sprite;
 
+exports.watching = watching;
 
 exports.build = series(cleanDist, building);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(styles, scripts, watching);
